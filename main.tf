eced2724e3629f687b49ef588573d8ec2f0dcdba
Achamyeleh <achamyeleh.mesfin@giz.de>
2018-07-31 18:58:00 +0300

    Adding Ansible as local-exec

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 resource "aws_instance" "my-test-instance" {
-  ami             = "${data.aws_ami.ubuntu.id}"
-  instance_type   = "t2.micro"
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
 
   tags {
     Name = "MPN"
+  }
+
+  provisioner "local-exec" {
+    command = "sleep 30 && echo -e \"[MPN]\n${aws_instance.MPN.ipv4_address} ansible_connection=ssh ansible_ssh_user=root\" > inventory &&  ansible-playbook -i inventory playbooks/vagrant.yml"
   }
 }
